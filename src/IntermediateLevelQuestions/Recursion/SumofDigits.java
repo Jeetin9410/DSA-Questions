5aa42f02fe890ff15c8649179b78d88d92f649c7
Jeetin kumar anand <34004214+Jeetin9410@users.noreply.github.com>
2022-06-22 12:24:33 +0530

    recursion

---
+++ src/IntermediateLevelQuestions/Recursion/SumofDigits.java
@@ -0,0 +1,31 @@
+package IntermediateLevelQuestions.Recursion;
+
+public class SumofDigits {
+    /*
+    Question :
+    Given a number A, we need to find the sum of its digits using recursion.
+
+    Problem Constraints
+    1 <= A <= 109
+
+    Example Input
+        Input 1: A = 46
+        Input 2: A = 11
+
+        Example Output
+        Output 1: 10
+        Output 2:  2
+
+     */
+
+    public static void main(String[] args) {
+        int A = 46;
+        System.out.println(sumOfDigits(A));
+    }
+    public static int sumOfDigits(int a){
+        if(a==0){
+            return 0;
+        }
+        return (a % 10 + sumOfDigits(a/10) );
+    }
+}
