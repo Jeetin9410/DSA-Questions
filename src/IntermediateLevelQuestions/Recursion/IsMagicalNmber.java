86e141483871ee21bd625d0e6d7a606d39705514
Jeetin kumar anand <34004214+Jeetin9410@users.noreply.github.com>
2022-06-29 20:20:52 +0530

    Traversal of LinkedList

--- src/IntermediateLevelQuestions/Recursion/IsMagicalNmber.java
+++ src/IntermediateLevelQuestions/Recursion/IsMagicalNmber.java
@@ -35,8 +35,7 @@ Output 2:  0
 
 */
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) {    
         int A = 83557;
         System.out.println(solve(A));
     }
