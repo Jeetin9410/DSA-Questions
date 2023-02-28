1cb1448dc876eaf62b31f45b5637912d7ff67dcf
Jeetin kumar anand <34004214+Jeetin9410@users.noreply.github.com>
2023-02-28 19:10:57 +0530

    Implement Power Function With Modulus

---
+++ src/AdvanceLevelQuestions/Mathematics/ImplementPOW_Function.java
@@ -0,0 +1,29 @@
+package AdvanceLevelQuestions.Mathematics;
+
+public class ImplementPOW_Function {
+    /*
+    Given two integers x and n, write a function to compute x^n.
+     We may assume that x and n are small and overflow doesn’t happen.
+     */
+    public static void main(String[] args) {
+        int x = -3;
+        int n = 1000000000;
+
+        System.out.println(pow(x,n));
+    }
+    public static int pow(int x, int n){
+        if(x==0)
+            return 0;
+
+        if(n==0)
+            return 1;
+
+        int ans = pow(x,n/2);
+
+        if(n%2 == 0){
+            return ans * ans;
+        }else {
+            return ans * ans * x;
+        }
+    }
+}
