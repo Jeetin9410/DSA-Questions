fd060f8c20ceefdbef7d809e957519bb1c8b827d
Jeetin kumar anand <34004214+Jeetin9410@users.noreply.github.com>
2023-03-24 20:38:16 +0530

    Merge Two sorted arrays

--- src/AdvanceLevelQuestions/Sorting/II_BubbleSort/BubbleSort.java
+++ src/AdvanceLevelQuestions/Sorting/II_BubbleSort/BubbleSort.java
@@ -9,22 +9,23 @@ public class BubbleSort {
     }
     public static void BubbleSort(int[] arr){
         int n = arr.length;
-        for (int i = 0; i < n ; i++) {
-            for (int j = i; j <n-1 ; j++) {
+        boolean swapped = false;
+        for (int i = 0; i < n-1 ; i++) {
+            for (int j = 0; j <n-i-1; j++) {
                 if(arr[j] > arr[j+1]){
                     int temp = arr[j+1];
                     arr[j+1] =arr[j];
                     arr[j] = temp;
+                    swapped = true;
                 }
+            }
+            if(swapped == false){
+                break;
             }
         }
         System.out.println(Arrays.toString(arr));
     }
 
-    /*              ISSUE IN ABOVE CODE
-
-
-
-     */
+  
 
 }
