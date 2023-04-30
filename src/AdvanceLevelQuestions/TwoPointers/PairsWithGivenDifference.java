a5ac89514fce2b718b735637e597f93a1cf38677
Jeetin kumar anand <34004214+Jeetin9410@users.noreply.github.com>
2023-04-30 21:59:01 +0530

    Completed : Pairs with given difference

--- src/AdvanceLevelQuestions/TwoPointers/PairsWithGivenDifference.java
+++ src/AdvanceLevelQuestions/TwoPointers/PairsWithGivenDifference.java
@@ -30,24 +30,39 @@ Input 3:
      */
 
     public static void main(String[] args) {
-            int[] A = {1,5,3,4,2};
+            int[] A = {8, 5, 1, 10, 5, 9, 9, 3, 5, 6, 6, 2, 8, 2, 2, 6, 3, 8, 7, 2, 5, 3, 4, 3, 3, 2, 7, 9, 6, 8, 7, 2, 9, 10, 3, 8, 10, 6, 5, 4, 2, 3};
             int B = 3;
 
             int n = A.length;
             int count = 0;
 
-            HashMap<Integer, Integer> map = new HashMap<>();
+            /// Optimal approach
+        /* Sorting the array first. Why? will get to next
+        we will take two pointers i  and j
+        at starting i =0;  j=i+1;
 
-            for(int i=0;i<A.length;i++){
-                int nextNum = (A[i] > B)?(A[i]-B):(A[i]+B);
-                if(map.containsKey(nextNum)){
-                    count += map.get(nextNum);
+        we will move j and check for two conditions
+        1) (A[j] - A[i]) < B  then move j++
+        2) (A[j] - A[i]) > B  then move i++
+
+         */
+
+        Arrays.sort(A);
+        int i =0;
+        int j=1;
+        while (i<A.length && j<A.length){
+                if((A[j] - A[i]) == B){
+                    count++;
+                   // break;
+                } else if((A[j] - A[i])<B){
+                    j++;
+                }else{
+                    i++;
                 }
-                map.put(A[i],map.getOrDefault(A[i],0)+1);
-            }
+
+        }
 
         System.out.println(count);
-
     }
 
 }
