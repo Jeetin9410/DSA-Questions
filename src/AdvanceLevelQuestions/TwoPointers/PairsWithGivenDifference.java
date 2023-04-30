fe5ef472118c2c5bec5a9fd39f2e79d0f675c315
Jeetin kumar anand <34004214+Jeetin9410@users.noreply.github.com>
2023-04-30 22:39:51 +0530

    Code changes

--- src/AdvanceLevelQuestions/TwoPointers/PairsWithGivenDifference.java
+++ src/AdvanceLevelQuestions/TwoPointers/PairsWithGivenDifference.java
@@ -30,7 +30,7 @@ Input 3:
      */
 
     public static void main(String[] args) {
-            int[] A = {1,5,3,4,2};
+            int[] A = {8, 5, 1, 10, 5, 9, 9, 3, 5, 6, 6, 2, 8, 2, 2, 6, 3, 8, 7, 2, 5, 3, 4, 3, 3, 2, 7, 9, 6, 8, 7, 2, 9, 10, 3, 8, 10, 6, 5, 4, 2, 3};
             int B = 3;
 
             int n = A.length;
@@ -40,8 +40,9 @@ Input 3:
 
             for(int i=0;i<A.length;i++){
                 int nextNum = (A[i] > B)?(A[i]-B):(A[i]+B);
-                if(map.containsKey(nextNum)){
-                    count += map.get(nextNum);
+                int num1 = A[i] + B;
+                if(map.containsKey(num1)){
+                    count++;
                 }
                 map.put(A[i],map.getOrDefault(A[i],0)+1);
             }
