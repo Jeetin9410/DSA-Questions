67a45e4044499c8d52327494f3f189ae34176236
Jeetin9410 <kumaranand8126@gmail.com>
2023-07-26 02:24:58 +0530

    Solved Longest Continuous Sequence with explaination

--- src/AdvanceLevelQuestions/Hashing/LongestContinuousSequence.java
+++ src/AdvanceLevelQuestions/Hashing/LongestContinuousSequence.java
@@ -2,6 +2,8 @@ package AdvanceLevelQuestions.Hashing;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 public class LongestContinuousSequence {
     /*
@@ -51,8 +53,10 @@ Explanation 2:
 
      */
     public static void main(String[] args) {
-        int[] A = {97,86,67,19,107,9,8,49,23,46,-4,22,72,4,57,111,20,52,99,2,113,81,7,5,21,0,47,54,76,117,-2,102,34,12,103,69,36,51,105,-3,33,91,37,11,48,106,109,45,58,77,104,60,75,90,3,62,16,119,85,63,87,43,74,13,95,94,56,28,55,66,92,79,27,42,70};
-        Arrays.sort(A);
+        int[] A = {100,4,200,3,2,1 } ;
+        /*
+     This approach won't work due to duplicates
+     Arrays.sort(A);
         System.out.println(Arrays.toString(A));
         int count = 1;
         for(int i =1;i<A.length;i++){
@@ -61,7 +65,77 @@ Explanation 2:
             }
         }
 
-        System.out.println(count);
+        System.out.println(count);*/
 
+
+        ////////25-07-2023////
+
+        ////////// This solution will also work
+
+        /// This idea came to my mind , due to Unordered nature of set.
+        /// Here I was trying to implement the solution using SortedSet which stores the values in sorted manner
+        //// Issue  here : Here Add() time complexity is O(logn) so for adding n element it will take nLogn, which itself disqualify this solution.
+
+       // SortedSet<Integer> set = new TreeSet<>();
+      /*  for(int val : A){
+            set.add(val);
+        }
+*/
+
+
+        /////////////  OPTIMAL SOLUTION  //////////////
+
+        /*
+
+        set = {100, 4 , 200, 3, 2, 1}
+              for 100 , we find 99, 101 in set
+              first we find 99, that does not exist, it means there is no previous number
+              so we check if 101 exist or not, if exist then we will keep checking the curent + 1 in set
+              this means 100 is the smallest one and after it all number are consecutive till we are able to find currentNum + 1 in set
+
+           so doing above task for i =0 i.e 100,  we find out that no previous number existed, and in case if 99  existed we will move to next number
+           Ques Why we are moving ? if we find previous smaller number
+           Ans : Reason is, if 99 already exist in set, so that means when we reach 99 we will also check for 99 +1 , and 100 will be there and if 100 exist
+                            we check 101, so no till the number not exist in set, and doing this process we also increment the length.
+
+           for i=1, i.e 4, --> 4-1 = 3 exist we will move to next number because we know when that numbwr will come we automaticaly reach to 4 just by doing+1
+           for i=2, i.e 200 --> 199 not exist, to there is chance that 200 might give us a longest consecutive number list, cause there is no shorter number just before 200 is available.
+                        so we check else case :
+                        here we will check if 200 + 1 exist or not, if exist then keep checking for next number in set untill number not exist in set condition occurs.
+          for i=3, i.e 3, --> 2 exist so move to next number
+          for i=4, i.e 2 --> 1 exist so move to next number.
+          for i=5, i.e 1, 0 not exist so check 2  len = 1
+                          2 exist --> len++, if 3 exist , len = 2
+                          3 exist --> len++ , if 4 exist, len = 3
+                          4 exist --> len++, if 5 exist,  len = 4
+                          5 not exist --> dont do anything
+         final len is answer.
+         */
+
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int val : A){
+            set.add(val);
+        }
+
+        int maxLen = 1; /// cause single element is also len 1
+
+        for(int i =0;i< A.length;i++){
+            int currentNum = A[i];
+            if(set.contains(currentNum - 1)){
+                continue;
+            }else {
+                int count =1;
+                int nextElement = currentNum+1;
+                while(set.contains(nextElement)){
+                    nextElement++;
+                    count++;
+                }
+                maxLen = Math.max(maxLen,count);
+            }
+        }
+
+        System.out.println(maxLen);
     }
 }
