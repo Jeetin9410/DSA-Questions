6035113e78b2a8fb728d19360ae7e8eb485b16f1
Jeetin kumar anand <kumaranand8126@gmail.com>
2023-08-12 17:45:31 +0530

    Relative Sort Array : Better to Modified Solution

--- src/AdvanceLevelQuestions/Hashing/RelativeSortArray.java
+++ src/AdvanceLevelQuestions/Hashing/RelativeSortArray.java
@@ -1,9 +1,6 @@
 package AdvanceLevelQuestions.Hashing;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
+import java.util.*;
 
 public class RelativeSortArray {
 /*
@@ -47,32 +44,124 @@ Each arr2[i] is in arr1.
         int[] arr2 = {2,1,4,3,9,6};
 
 
-        HashMap<Integer,Integer> map = new HashMap<>();
+      /*  HashMap<Integer,Integer> map = new HashMap<>();
 
         for(int val : arr1){
               map.put(val, map.getOrDefault(val, 0) + 1);
         }
 
-        System.out.println(map.toString());
-
-        ArrayList<Integer> ans = new ArrayList<>();
-        ArrayList<Integer> notCommon = new ArrayList<>();
-
-        for(int val : arr2){
-            if(map.containsKey(val)){
-                int count = map.get(val);
-                while(count>0){
-                    ans.add(val);
-                    count--;
+        int[] ans = new int[arr1.length];
+        int index = 0;
+        TreeMap<Integer, Integer> tm = new TreeMap<>(map);
+        for(int i : arr2){
+            if(tm.containsKey(i)){
+                for(int j = 0; j < tm.get(i); j++){
+                    ans[index++] = i;
                 }
-                map.remove(val);
+                tm.remove(i);
+            }
+        }
+        for (int i : tm.keySet()) {
+            int freq = tm.get(i);
+            for (int j = 0; j < freq; j++) {
+                ans[index++] = i;
+            }
+        }
+        System.out.println(Arrays.toString(ans));
+
+
+        */
+       // Issue in above solution was that because HashMap does not save values in ordered way
+       // So We need to solve that using TreeMap also
+
+
+     /*   HashMap<Integer, Integer> hm = new HashMap<>();
+        for(int i : arr1){
+            hm.put(i, hm.getOrDefault(i,0)+1);
+        }
+        int[] ans = new int[arr1.length];
+        int index = 0;
+        TreeMap<Integer, Integer> tm = new TreeMap<>(hm);
+        for(int i : arr2){
+            if(tm.containsKey(i)){
+                for(int j = 0; j < tm.get(i); j++){
+                    ans[index++] = i;
+                }
+                tm.remove(i);
+            }
+        }
+        for (int i : tm.keySet()) {
+            int freq = tm.get(i);
+            for (int j = 0; j < freq; j++) {
+                ans[index++] = i;
+            }
+        }
+        System.out.println(Arrays.toString(ans));
+    */
+        //The above solution works perfect but only issue is that it is not optimised
+
+        //////////////////// Method 3 : Using Arrays Only ///////////////////////
+
+        /*
+
+        The given problem asks to sort the elements of arr1 such that their relative ordering matches
+        that of arr2. Here's a detailed description of the approach used in the provided code:
+
+1) Initialize an array arr of size 1001 to count the occurrences of each element in arr1.
+The index of arr represents the element value, and the value at that index represents the count.
+
+2) Traverse through arr1 and increment the count in arr for each element encountered.
+
+3) Create a result array res of the same length as arr1.
+
+4) Initialize a variable i to keep track of the current index in res.
+
+5) Traverse through arr2 to maintain the relative ordering specified by arr2.
+    a) For each element num in arr2, while the count of num in arr is greater than 0:
+            --Set res[i] to num.
+            --Decrement the count of num in arr.
+            --Increment i.
+6) After processing all elements in arr2, iterate through arr to handle the remaining elements
+that were not in arr2.
+    a) For each index j in arr, while the count at arr[j] is greater than 0:
+            --Set res[i] to j.
+            --Decrement the count of j in arr.
+            --Increment i.
+7) Return the resulting array res.
+
+         */
+
+        /// Below solution is only possible
+       /// 0 <= arr1[i], arr2[i] <= 1000
+       //// If this was higher number then TreeMap will be the optimal solution.
+        int[] arr = new int[1001];
+
+        for(int num : arr1)
+            arr[num]++;
+
+        System.out.println(Arrays.toString(arr));
+
+        int[] res = new int[arr1.length];
+
+        int i = 0;
+
+        for(int num : arr2){
+            while(arr[num] > 0){
+                res[i] = num;
+                arr[num]--;
+                i++;
             }
         }
 
-       map.forEach((key,value)->{
-           ans.add(key);
-       });
+        for(int j = 0; j < arr.length; j++){
+            while(arr[j] > 0){
+                res[i] = j;
+                arr[j]--;
+                i++;
+            }
+        }
 
-        System.out.println(Arrays.toString(ans.toArray()));
+      //  System.out.println(Arrays.toString(res));
+
     }
 }
--- src/IntermediateLevelQuestions/Arrays/Introduction/Kth_SmallestNumberLeetCode.java
+++
@@ -1,4 +0,0 @@
-package IntermediateLevelQuestions.Arrays.Introduction;
-
-public class Kth_SmallestNumberLeetCode {
-}
