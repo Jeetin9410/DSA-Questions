1bd096654d825cb7324eadacccde2591d6826829
Jeetin9410 <34004214+Jeetin9410@users.noreply.github.com>
2025-01-01 23:32:56 +0530

    feat(leetcode): 1 Jan daily question

---
+++ src/DailyLeetCode2025/Jan/Day1_Maximum_Score_After_Splitting_String.kt
@@ -0,0 +1,35 @@
+package DailyLeetCode2025.Jan
+
+//link : https://leetcode.com/problems/maximum-score-after-splitting-a-string?envType=daily-question&envId=2025-01-01
+
+fun main() {
+    val s = "00"
+
+    /*
+    00
+    0 0
+    1 0
+    1
+     */
+
+    val count = IntArray(s.length)
+    var totalOnes = 0;
+    s.forEachIndexed { index, c ->
+        if(c == '1') {
+            totalOnes++
+        }
+        count[index] = totalOnes
+    }
+
+    var max = 0;
+
+    count.forEachIndexed { index, totalOnesTillNow ->
+        if (index < s.length - 1) { // Ensure non-empty right substring
+            val zerosInLeft = (index + 1) - totalOnesTillNow
+            val onesInRight = totalOnes - totalOnesTillNow
+            max = maxOf(max, zerosInLeft + onesInRight)
+        }
+    }
+
+    println(max)
+}
