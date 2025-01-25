e030ef89dd22033c55aa9a850fac67383ba962e2
Jeetin9410 <34004214+Jeetin9410@users.noreply.github.com>
2025-01-25 12:56:15 +0530

    feat

---
+++ src/AdvanceLevelQuestions/Arrays/KadaneAlgoWithVariations/MaximumProductSubArray.java
@@ -0,0 +1,50 @@
+package AdvanceLevelQuestions.Arrays.KadaneAlgoWithVariations;
+
+public class MaximumProductSubArray {
+    /*
+    Given an integer array nums, find a subarray that has the largest product, and return the product.
+
+    The test cases are generated so that the answer will fit in a 32-bit integer.
+
+    Example 1:
+
+    Input: nums = [2,3,-2,4]
+    Output: 6
+    Explanation: [2,3] has the largest product 6.
+    Example 2:
+
+    Input: nums = [-2,0,-1]
+    Output: 0
+    Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+
+
+    Constraints:
+
+    1 <= nums.length <= 2 * 104
+    -10 <= nums[i] <= 10
+    The product of any subarray of nums is guaranteed to fit in a 32-bit integer.
+     */
+    public static void main(String[] args) {
+
+        //Explanation: https://www.youtube.com/watch?v=hnswaLJvr6g&ab_channel=takeUforward
+
+        int[] nums = {5,-3,2,4,1};
+
+        int n = nums.length;
+        int prefix = 1;
+        int suffix = 1;
+        int product = Integer.MIN_VALUE;
+
+        for (int i =0; i < n; i++) {
+            if(prefix == 0) prefix = 1;
+            if(suffix == 0) suffix = 1;
+
+            prefix = prefix * nums[i];
+            suffix = suffix * nums[n-i-1];
+
+            product = Math.max(product , Math.max(prefix,suffix));
+        }
+
+        System.out.println(product);
+    }
+}
