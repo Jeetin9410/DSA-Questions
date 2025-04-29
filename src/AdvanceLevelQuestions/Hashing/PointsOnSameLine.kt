d4f4c050f25f49cbe8374ac01ef1e09c97a22cae
Jeetin9410 <34004214+Jeetin9410@users.noreply.github.com>
2025-04-29 22:21:33 +0530

    feat: recursion basics practice

---
+++ src/AdvanceLevelQuestions/Hashing/PointsOnSameLine.kt
@@ -0,0 +1,22 @@
+package AdvanceLevelQuestions.Hashing
+
+fun main() {
+
+}
+
+fun solve(A: IntArray, B: IntArray): Int {
+    // Idea: All the point that lies on a line will have same slope(m) from (0,0)
+    // Will store slope to no of points on it in a map.
+
+    val map = hashMapOf<Float, Int>()
+    val len = A.size
+    var ans = 0
+    for(i in 0 until len) {
+        val slope = if(B[i] != 0) (A[i] / B[i]).toFloat() else 0.toFloat();
+        val count = map.getOrDefault(slope, 0) + 1
+        ans = Math.max(count,ans)
+        map.put(slope, count);
+    }
+
+    return ans
+}
---
+++ src/DsaPatterns/Recursion/Basic/Prob5_IsReverse.java
@@ -0,0 +1,29 @@
+package DsaPatterns.Recursion.Basic;
+
+public class Prob5_IsReverse {
+    /*
+    You are given two strings str1 and str2 and you have to return true if str2 is reverse of str1,
+    otherwise return false.
+    */
+
+    public static void main(String[] args) {
+        String str1 = "cat";
+        String str2 = "tacx";
+        System.out.println(isReverse(str1, str2));
+    }
+
+    public static boolean isReverse(String str1, String str2) {
+        if(str1.isEmpty() && str2.isEmpty()) {
+            return true;
+        }
+
+        if(str1.length() != str2.length()) {
+            return false;
+        }
+
+        String first = str1.substring(0,1);
+        String last = str2.substring(str2.length()-1);
+
+        return first.equals(last) && isReverse(str1.substring(1), str2.substring(0, str2.length() -1));
+    }
+}
